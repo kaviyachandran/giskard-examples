e1e2629acc42b0d1e3fdd81d6f29b629d0c20bdd
¨kaviyachandran¨ <kaviya.dhanabalachandran@gmail.com>
2024-09-09 16:42:56 +0200

    Add knowrob dependencies

--- binder/entrypoint.sh
+++ binder/entrypoint.sh
@@ -3,4 +3,15 @@ source ${ROS_WS}/devel/setup.bash
 roscore &
 roslaunch --wait rvizweb rvizweb.launch interactive_markers:=True &
 
+# Start MongoDB and save data on working directory
+MONGODB_URL=mongodb://127.0.0.1:27017
+# Store MongoDB data under directory ${HOME}/data/db
+mongod --fork --logpath ${HOME}/mongod.log
+
+# jupyterlab UI workspace
+jupyter lab workspaces import ${PWD}/binder/jupyterlab.jupyterlab-workspace
+
+# Launch Knowrob
+export KNOWROB_MONGODB_URI=${MONGODB_URL}/?appname=knowrob
+
 exec "$@"
